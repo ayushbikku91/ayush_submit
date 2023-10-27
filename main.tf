16a2df5362dc72610b65673d06e3823049a5cf18
Ayush Parth <54761726+ayushbikku91@users.noreply.github.com>
2023-10-27 22:06:05 +0530

    Delete main.tf
    Delete wrong files

--- main.tf
+++
@@ -1,7 +0,0 @@
-provider "google" {
-  credentials = file("<YOUR_SERVICE_ACCOUNT_KEY_FILE>")
-  project     = "<YOUR_PROJECT_ID>"
-  region      = "us-central1"
-}
-
-# we can define variables and modules here as needed.
